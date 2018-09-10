9151d231095b73fee2e30456e5c2007236a58815
tak1827 <re795h@gmail.com>
2018-09-10 16:56:38 +0000

    Create sample todo app

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import FilterLink from '../containers/FilterLink'
+import { VisibilityFilters } from '../actions'
+
+const Footer = () => (
+  <div>
+    <FilterLink filter={VisibilityFilters.SHOW_ALL} id="show-all">
+      Show All
+    </FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE} id="show-active">
+      Show Active
+    </FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED} id="show-completed">
+      Show Completed
+    </FilterLink>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Link = ({ active, id, children, onClick }) => (
+  <div>
+    <input type="radio" name="visibility" id={id} value="all" checked={active ? "checked" : false} onChange={()=>{}}/>
+    <label htmlFor={id} style={{ marginRight: '32px'}} onClick={onClick}>{children}</label>
+  </div>
+)
+
+Link.propTypes = {
+  active: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default Link
